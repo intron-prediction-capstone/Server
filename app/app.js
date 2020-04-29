507b68aeb34f5f8eff14a4154936118ee4b935e2
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2020-04-29 14:37:35 -0400

    Uploading app and parser

---
+++ app/app.js
@@ -0,0 +1,280 @@
+/*
+Kyle Gorman and Will Eccles
+SER492 Senior Capstone
+Major and Minor Intron Dataset
+Rest api and server
+*/
+const express = require('express'); // Web Framework
+const app = express();
+const sql = require('mssql')
+
+var config = {
+    server: 'DESKTOP-J6DUC0L\\MSSQLSERVER19',
+    database: 'master',
+    user: 'adminLogin',
+    password: 'admin'
+};
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("app listening at http://%s:%s", host, port)
+});
+
+app.get('/api/gene', function (req, res) {
+    sql.connect(config, function () {
+        var request = new sql.Request();
+        request.query('select * from [IntronDB].[dbo].[Gene]', function (err, recordset) {
+            if (err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
+});
+
+app.get('/api/exon', function (req, res) {
+    sql.connect(config, function () {
+        var request = new sql.Request();
+        request.query('select * from [IntronDB].[dbo].[Exon]', function (err, recordset) {
+            if (err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
+});
+
+app.get('/api/intron', function (req, res) {
+    sql.connect(config, function () {
+        var request = new sql.Request();
+        request.query('select * from [IntronDB].[dbo].[Intron]', function (err, recordset) {
+            if (err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
+});
+
+app.get('/api/score', function (req, res) {
+    sql.connect(config, function () {
+        var request = new sql.Request();
+        request.query('select * from [IntronDB].[dbo].[Score]', function (err, recordset) {
+            if (err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
+});
+
+app.get('/api/species', function (req, res) {
+    sql.connect(config, function () {
+        var request = new sql.Request();
+        request.query('select * from [IntronDB].[dbo].[Species]', function (err, recordset) {
+            if (err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
+});
+
+app.get('/api/transcript', function (req, res) {
+    sql.connect(config, function () {
+        var request = new sql.Request();
+        request.query('select * from [IntronDB].[dbo].[Transcript]', function (err, recordset) {
+            if (err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
+});
+
+app.get('/api/search', function (req, res) {
+
+    var spInput = req.query.speciesName;
+    var vInput = req.query.version;
+    var egInput = req.query.ensemblGene;
+    var etInput = req.query.ensemblTranscript;
+    var gsInput = req.query.geneSymbol;
+    var icInput = req.query.intronClass;
+    var lInput = req.query.length;
+    var sInput = req.query.strand;
+    var sqInput = req.query.sequence;
+    var rInput = req.query.rank;
+
+    // connect to database
+    var conn = new sql.ConnectionPool(config);
+    conn.connect().then(function () {
+
+        // creats request object
+        var request = new sql.Request(conn);
+
+        // query to the database
+        request
+            //.query("select * from ")
+            .input('SpeciesName', sql.NVarChar(255), spInput)
+            .input('Version', sql.NVarChar(255), vInput)
+            .input('EnsemblGeneId', sql.VarChar(), egInput)
+            .input('EnsemblTranscriptId', sql.NVarChar(255), etInput)
+            .input('GeneSymbol', sql.VarChar(), gsInput)
+            .input('IntronClass', sql.Bit(), icInput)
+            .input('Length', sql.Int(), lInput)
+            .input('Strand', sql.Bit(), sInput)
+            .input('Sequence', sql.VarChar(), sqInput)
+            .input('Rank', sql.Int(), rInput)
+            .execute('[IntronDB].[dbo].[SearchProcedure]')
+            .then(function (recordset) {
+                // sends records as a response
+                res.end(JSON.stringify(recordset)); // Result in JSON format
+                conn.close();
+            }).catch(function (err) {
+                console.log(err);
+                conn.close();
+            });
+    }).catch(function (err) {
+        console.log(err);
+    });
+})
+
+
+app.get('/api/insert', function (req, res) {
+
+    var speciesName = req.query.speciesName;
+    var commonName = req.query.commonName;
+    var genomeVersion = req.query.genomeVersion;
+    var ensemblVersion = req.query.ensemblVersion;
+    var geneName = req.query.geneName;
+    var ncbiGeneId = req.query.ncbiGeneId;
+    var ensemblGeneId = req.query.ensemblGeneId;
+    var geneType = req.query.geneType;
+    var geneStartCoord = req.query.geneStartCoord;
+    var geneEndCoord = req.query.geneEndCoord;
+    var geneLength = req.query.geneLength;
+    var geneSequence = req.query.geneSequence;
+    var ncbiGeneLink = req.query.ncbiGeneLink;
+    var ensemblGeneLink = req.query.ensemblGeneLink;
+    var ucscLink = req.query.ucscLink;
+    var transcriptEnsemblLink = req.query.transcriptEnsemblLink;
+    var transcriptStartCoord = req.query.transcriptStartCoord;
+    var transcriptEndCoord = req.query.transcriptEndCoord;
+    var intronType = req.query.intronType;
+    var subtype = req.query.subtype;
+    var intronStartCoord = req.query.intronStartCoord;
+    var intronSequence = req.query.intronSequence;
+    var rank = req.query.rank;
+    var intronLength = req.query.intronLength;
+    var branchPoint = req.query.branchPoint;
+    var acceptorSpliceSite = req.query.acceptorSpliceSite;
+    var strand = req.query.strand;
+    var cluster = req.query.cluster;
+    var frame = req.query.frame;
+    var chromosome = req.query.chromosome;
+    var intronEndCoord = req.query.intronEndCoord;
+    var donorSpliceSite = req.query.donorSpliceSite;
+    var overallScore = req.query.overallScore;
+    var fiveScore = req.query.fiveScore;
+    var threeScore = req.query.threeScore;
+    var breakPointScore = req.query.breakPointScore;
+    var scoreType = req.query.scoreType;
+    var exonStartCoordUp = req.query.exonStartCoordUp;
+    var exonEndCoordUp = req.query.exonEndCoordUp;
+    var exonStartCoordDown = req.query.exonStartCoordDown;
+    var exonEndCoordDown = req.query.exonEndCoordDown;
+    var exonType = req.query.exonType;
+
+    // connect to database
+    var conn = new sql.ConnectionPool(config);
+    conn.connect().then(function () {
+
+        // creats request object
+        var request = new sql.Request(conn);
+
+        // query to the database
+        request
+            //.query("select * from ")
+            .input('speciesName', sql.NVarChar(255), speciesName)
+            .input('commonName', sql.NVarChar(255), commonName)
+            .input('genomeVersion', sql.NVarChar(255), genomeVersion)
+            .input('ensemblVersion', sql.NVarChar(255), ensemblVersion)
+
+            .input('geneName', sql.VarChar(), geneName)
+            .input('ncbiGeneID', sql.VarChar(), ncbiGeneId)
+            .input('ensemblGeneId', sql.VarChar(), ensemblGeneId)
+            .input('geneType', sql.VarChar(), geneType)
+            .input('geneStartCoord', sql.BigInt, geneStartCoord)
+            .input('geneEndCoord', sql.BigInt(), geneEndCoord)
+            .input('geneLength', sql.Int(), geneLength)
+            .input('geneSequence', sql.NVarChar(255), geneSequence)
+            .input('ncbiGeneLink', sql.NVarChar(255), ncbiGeneLink)
+            .input('ensemblGeneLink', sql.NVarChar(255), ensemblGeneLink)
+            .input('ucscLink', sql.NVarChar(255), ucscLink)
+
+            .input('transcriptEnsemblLink', sql.NVarChar(255), transcriptEnsemblLink)
+            .input('transcriptStartCoord', sql.BigInt(), transcriptStartCoord)
+            .input('transcriptEndCoord', sql.BigInt(), transcriptEndCoord)
+
+            .input('intronType', sql.Bit(), intronType)
+            .input('subtype', sql.VarChar(5), subtype)
+            .input('intronStartCoord', sql.BigInt(), intronStartCoord)
+            .input('intronSequence', sql.VarChar(), intronSequence)
+            .input('rank', sql.Int(), rank)
+            .input('intronLength', sql.Int(), intronLength)
+            .input('branchPoint', sql.VarChar(), branchPoint)
+            .input('acceptorSpliceSite', sql.VarChar(), acceptorSpliceSite)
+            .input('strand', sql.Bit(), strand)
+            .input('cluster', sql.Int(), cluster)
+            .input('frame', sql.Bit(), frame)
+            .input('chromosome', sql.VarChar(), chromosome)
+            .input('intronEndCoord', sql.BigInt(), intronEndCoord)
+            .input('donorSpliceSite', sql.VarChar(), donorSpliceSite)
+
+            .input('overallScore', sql.Float(), overallScore)
+            .input('fiveScore', sql.Float(), fiveScore)
+            .input('threeScore', sql.Float(), threeScore)
+            .input('breakPointScore', sql.Float(), breakPointScore)
+            .input('scoreType', sql.VarChar(255), scoreType)
+
+            .input('exonStartCoordUp', sql.BigInt(), exonStartCoordUp)
+            .input('exonEndCoordUp', sql.BigInt(), exonEndCoordUp)
+            .input('exonStartCoordDown', sql.BigInt(), exonStartCoordDown)
+            .input('exonEndCoordDown', sql.BigInt(), exonEndCoordDown)
+            .input('exonType', sql.VarChar(), exonType)
+
+            .execute('[IntronDB].[dbo].[InsertData]')
+            .then(function (recordset) {
+                // sends records as a response
+                res.end(JSON.stringify(recordset)); // Result in JSON format
+                console.log('insert complete');
+                conn.close();
+            }).catch(function (err) {
+                console.log(err);
+                conn.close();
+            });
+    }).catch(function (err) {
+        console.log(err);
+    });
+})
+
+
+app.get('/api/insertGene', function (req, res) {
+
+    var ensemblGeneId = req.query.ensemblGeneId;
+    var ensemblTranscriptId = req.query.ensemblTranscriptId;
+    var intronStartCoord = req.query.intronStartCoord;
+    var intronEndCoord =req.query.intronEndCoord;
+
+    sql.connect().then(function () {
+        // creats request object
+        var request = new sql.Request(conn);
+        // query to the database
+        request
+            .input('ensemblGeneId', sql.VarChar(), ensemblGeneId)
+            .input('ensemblTranscriptId', sql.NVarChar(255), ensemblTranscriptId)
+            .input('intronStartCoord', sql.BigInt, intronStartCoord)
+            .input('intronEndCoord', sql.BigInt, intronEndCoord)
+            .input('intronSequence', sql.VarChar, intronSequence)
+            .execute('[InrtonDB].[dbo].[InsertGeneSequence]')
+            .then(function (recordset) {
+                console.log('fasta: insert complete ' + counter);
+                conn.close();
+            }).catch(function (err) {
+                console.log('Error inserting ' + err);
+                conn.close();
+            });
+    }).catch(function (err) {
+        console.log('Error connecting: ' + err);
+    });
+});
---
+++ app/parser.js
@@ -0,0 +1,261 @@
+/*
+Kyle Gorman and Will Eccles
+SER492 Senior Capstone
+Major and Minor Intron Dataset
+Parser for inputting into database
+*/
+const express = require('express'); // Web Framework
+const app = express();
+const sql = require('mssql')
+
+var config = {
+    server: 'DESKTOP-J6DUC0L\\MSSQLSERVER19',
+    database: 'master',
+    user: 'adminLogin',
+    password: 'admin'
+};
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("app listening at http://%s:%s", host, port)
+});
+
+const LineByLineReader = require('line-by-line'),
+    lr0 = new LineByLineReader('../input/input gtf.gtf'),
+    lr1 = new LineByLineReader('../input/input fasta.fa');
+
+//number of lines to read in
+//if set to -1 will run entire file
+var lines = -1;
+var counter0 = 0;
+
+//reads in gtf files
+lr0.on('line', (line) => {
+    //pause emitting of lines
+    lr0.pause();
+    //breaks the input into an array
+    var res = line.split('\t');
+
+    //sets the last introns coord 
+    var lastIntronStartCoord = 0;
+    var lastIntronEndCoord = 0;
+
+    console.log('gtf: start ' + counter0);
+    //synchronous line processing
+    setTimeout(function () {
+        insertData(res, lastIntronStartCoord, lastIntronEndCoord, counter0);
+        //continue emitting lines.
+        lr0.resume();
+    }, 1000)
+
+    //ends after x lines
+    counter0++;
+    if (counter0 > lines - 1 && lines != -1) {
+        //closes all listeners
+        lr0.close();
+        lr0.removeAllListeners();
+    }
+});
+lr0.on('end', function () {
+    // All lines are read, file is closed now.
+    lr0.close();
+    lr0.removeAllListeners();
+});
+
+//reads in fasta files
+//each gene sequence is considered a "line" not each actual line in the file
+//the gene sequences are seperated by comments that begin with >
+var counter1 = 0;
+var linecount = 0;
+var str = '';
+var header = '';
+lr1.on('line', (line) => {
+    //console.log(line);
+    if(counter1 == 0) {
+        header = line;
+        counter1++
+    } else if (line.charAt(0) == '>') {
+        lr1.pause();
+        console.log('fasta: start ' + linecount);
+        //console.log('Header: ' + header);
+        //console.log('String: ' + str);
+
+        //handles inserts for the fasta file
+        setTimeout(function () {
+            linecount++;
+            insertGeneSeq(header, str, linecount);
+            //continue emitting lines.
+            header = line;
+            str = '';
+            counter1++
+            lr1.resume();
+        }, 1000)
+       
+    } else {
+        str = str + line;
+    }
+    if (counter1 - 1 > lines - 1  && lines != -1) {
+        lr1.close();
+        lr1.removeAllListeners();
+    }
+
+});
+lr1.on('end', function () {
+    // All lines are read, file is closed now.
+    lr1.close();
+    lr1.removeAllListeners();
+});
+
+//handles inserting the gene sequence
+function insertGeneSeq(header, str, counter) {
+    //splits the header itno each of the fields
+    var res = header.split(',');
+    var coords = res[2].split('-');
+    var ensemblGeneId = res[0].substring(1, res[0].length);
+    var ensemblTranscriptId = res[1];
+    var intronStartCoord = coords[0];
+    var intronEndCoord = coords[1];
+
+    var intronSequence = str;
+    //connect to database
+    var conn = new sql.ConnectionPool(config);
+
+    conn.connect().then(function () {
+        // creats request object
+        var request = new sql.Request(conn);
+        // query to the database
+        request
+            .input('ensemblGeneId', sql.VarChar(), ensemblGeneId)
+            .input('ensemblTranscriptId', sql.NVarChar(255), ensemblTranscriptId)
+            .input('intronStartCoord', sql.BigInt, intronStartCoord)
+            .input('intronEndCoord', sql.BigInt, intronEndCoord)
+            .input('intronSequence', sql.VarChar, intronSequence)
+            .execute('[IntronDB].[dbo].[InsertGeneSequence]')
+            .then(function (recordset) {
+                console.log('fasta: insert complete ' + counter);
+                conn.close();
+            }).catch(function (err) {
+                console.log('Error inserting ' + err);
+                conn.close();
+            });
+    }).catch(function (err) {
+        console.log('Error connecting: ' + err);
+    });
+
+}
+
+function insertData(res, lastIntronStartCoord, lastIntronEndCoord, counter) {
+    var speciesName = 'Homo sapien';
+    var commonName = 'Human';
+
+    //GTF file format is seqname, source, feature, start, end, score, strand, frame, geneid, transcriptid,
+    //takes the input and splits it by tabs
+    //inserts the data into the database
+    var chromosome = res[0];
+    var scoreType = res[1];
+    var feature = res[2];
+    var intronStartCoord = res[3];
+    var intronEndCoord = res[4];
+    var overallScore = res[5];
+    if (res[6] == '+')
+        var strand = 1;
+    else
+        var strand = 0;
+    //frame is stored as a bit ./+ is 0 and - is 1
+    var frame;
+    if (res[7] == '-')
+        frame = 1;
+    else
+        frame = 0;
+    //the 8th item in the array ocntain the gene id and the transcript id
+    //splits the 8th item of the array at the comma then makes two substrings of each
+    var ensemblGeneId = res[9].split(';')[0].substring(9, res[9].split(';')[0].length - 1);
+    var transcriptId = res[9].split(';')[1].substring(16, res[9].split(';')[1].length - 1);
+
+    var ensemblGeneLink = "https://useast.ensembl.org/Homo_sapiens/Gene/Summary?g=" + ensemblGeneId;
+    var intronLength = intronEndCoord - intronStartCoord;
+    var transcriptEnsemblLink = transcriptId;
+    //stored as a bit 0 is U2 1 is U12
+    var intronType;
+    if (res[5] > 25)
+        intronType = 1;
+    else if (res[5] == -1)
+        intronType = null;
+
+    //sets the exon cord and type for downstream
+    var exonStartCoordUp = lastIntronEndCoord + 1;
+    var exonEndCoordUp = lastIntronStartCoord - 1;
+    var exonStartCoordDown = lastIntronEndCoord + 1;
+    var exonEndCoordDown = lastIntronStartCoord - 1;
+
+    //connect to database
+    var conn = new sql.ConnectionPool(config);
+
+    conn.connect().then(function () {
+        // creats request object
+        var request = new sql.Request(conn);
+        // query to the database
+        request
+            //.query("select * from ")
+            .input('speciesName', sql.NVarChar(255), speciesName)
+            .input('commonName', sql.NVarChar(255), commonName)
+            // .input('genomeVersion', sql.NVarChar(255), genomeVersion)
+            // .input('ensemblVersion', sql.NVarChar(255), ensemblVersion)
+
+            //.input('geneName', sql.VarChar(), geneName)
+            //.input('ncbiGeneID', sql.VarChar(), ncbiGeneId)
+            .input('ensemblGeneId', sql.VarChar(), ensemblGeneId)
+            //.input('geneType', sql.VarChar(), geneType)
+            //.input('geneStartCoord', sql.BigInt, geneStartCoord)
+            //.input('geneEndCoord', sql.BigInt(), geneEndCoord)
+            //.input('geneLength', sql.Int(), geneLength)
+            //.input('geneSequence', sql.NVarChar(255), geneSequence)
+            //.input('ncbiGeneLink', sql.NVarChar(255), ncbiGeneLink)
+            .input('ensemblGeneLink', sql.NVarChar(255), ensemblGeneLink)
+            //.input('ucscLink', sql.NVarChar(255), ucscLink)
+
+            .input('transcriptEnsemblLink', sql.NVarChar(255), transcriptEnsemblLink)
+            //.input('transcriptStartCoord', sql.BigInt(), transcriptStartCoord)
+            //.input('transcriptEndCoord', sql.BigInt(), transcriptEndCoord)
+            .input('ensemblId', sql.NVarChar(255), transcriptId)
+
+            .input('intronType', sql.Bit(), intronType)
+            //.input('subtype', sql.VarChar(5), subtype)
+            .input('intronStartCoord', sql.BigInt(), intronStartCoord)
+            //.input('intronSequence', sql.VarChar(), intronSequence)
+            //.input('rank', sql.Int(), rank)
+            .input('intronLength', sql.Int(), intronLength)
+            //.input('branchPoint', sql.VarChar(), branchPoint)
+            //.input('acceptorSpliceSite', sql.VarChar(), acceptorSpliceSite)
+            .input('strand', sql.Bit(), strand)
+            //.input('cluster', sql.Int(), cluster)
+            .input('frame', sql.Bit(), frame)
+            .input('chromosome', sql.VarChar(), chromosome)
+            .input('intronEndCoord', sql.BigInt(), intronEndCoord)
+            //.input('donorSpliceSite', sql.VarChar(), donorSpliceSite)
+
+            .input('overallScore', sql.Float(), overallScore)
+            //.input('fiveScore', sql.Float(), fiveScore)
+            //.input('threeScore', sql.Float(), threeScore)
+            //.input('breakPointScore', sql.Float(), breakPointScore)
+            .input('scoreType', sql.VarChar(255), scoreType)
+
+            .input('exonStartCoordUp', sql.BigInt(), exonStartCoordUp)
+            .input('exonEndCoordUp', sql.BigInt(), exonEndCoordUp)
+            .input('exonStartCoordDown', sql.BigInt(), exonStartCoordDown)
+            .input('exonEndCoordDown', sql.BigInt(), exonEndCoordDown)
+
+            .execute('[IntronDB].[dbo].[InsertData]')
+            .then(function (recordset) {
+                console.log('gtf: insert complete ' + counter);
+                conn.close();
+            }).catch(function (err) {
+                console.log('Error inserting ' + err);
+                conn.close();
+            });
+    }).catch(function (err) {
+        console.log('Error connecting: ' + err);
+    });
+}
